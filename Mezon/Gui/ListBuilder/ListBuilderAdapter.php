be9d50cb3b8ce2d66243ebccc7bbcdc35b9a49a0
gdever <alexey@dodonov.pro>
2020-12-04 09:29:54 +0300

    feature/Last fixes

--- Mezon/Gui/ListBuilder/ListBuilderAdapter.php
+++ Mezon/Gui/ListBuilder/ListBuilderAdapter.php
@@ -20,9 +20,9 @@ interface ListBuilderAdapter
     /**
      * Method returns all vailable records
      *
-     * @return array all vailable records
+     * @return object all vailable records
      */
-    public function all(): array;
+    public function all(): object;
 
     /**
      * Method returns a subset from vailable records
@@ -33,16 +33,16 @@ interface ListBuilderAdapter
      *            the beginning of the bunch
      * @param int $limit
      *            the size of the batch
-     * @return array subset from vailable records
+     * @return object subset from vailable records
      */
-    public function getRecords(array $order, int $from, int $limit): array;
+    public function getRecords(array $order, int $from, int $limit): object;
 
     /**
      * Record preprocessor
      *
      * @param object $record
      *            record to be preprocessed
-     * @return array preprocessed record
+     * @return object preprocessed record
      */
-    public function preprocessListItem(object $record): array;
+    public function preprocessListItem(object $record): object;
 }
